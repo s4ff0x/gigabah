52a7274001092a1f92c9bb7676be97d5126052ee
milanjrodd <milanrodd@gmail.com>
2025-07-05 23:20:39 +0300

    fix high network rate

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,7 @@ class_name Player
 ## Movement parameters
 @export var camera: Camera2D
 @export var speed: float = 400.0
+@export var move_direction: Vector2 = Vector2.ZERO
 
 func _enter_tree() -> void:
 	set_multiplayer_authority(name.to_int())
@@ -11,7 +12,8 @@ func _enter_tree() -> void:
 		camera.enabled = false
 
 func _physics_process(_delta: float) -> void:
-	if !is_multiplayer_authority(): return
-
-	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * speed
+	if is_multiplayer_authority():
+		move_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	velocity = move_direction * speed
 	move_and_slide()
